ac1e6b565a5a2b2b5a0b7982d1d55015993c6f5c
MingseoYa <alstj3758@naver.com>
2025-01-19 16:44:24 +0900

    ✨ Feat: 폼 공통컴포넌트 생성

---
+++ src/commons/components/form/index.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { ZodSchema } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  DefaultValues,
+  FieldValues,
+  FormProvider,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
+
+interface IForm<T extends FieldValues> {
+  children: React.ReactNode;
+  schema: ZodSchema<T>;
+  defaultValue?: DefaultValues<T>;
+  useInitialize: (methods: UseFormReturn<T>) => {
+    onSubmit: (data: T) => void;
+  };
+}
+
+export default function Form<T extends FieldValues>({
+  children,
+  schema,
+  defaultValue,
+  useInitialize,
+}: IForm<T>) {
+  console.log("schema:::", schema);
+  const method = useForm<T>({
+    resolver: zodResolver(schema),
+    mode: "all",
+    defaultValues: defaultValue,
+  });
+
+  const { onSubmit } = useInitialize(method);
+
+  return (
+    <FormProvider {...method}>
+      <form
+        onSubmit={method.handleSubmit(onSubmit)}
+        style={{
+          width: "100%",
+        }}
+      >
+        {children}
+      </form>
+    </FormProvider>
+  );
+}
