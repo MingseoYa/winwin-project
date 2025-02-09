d7ae88309097eac4e563102e4b905a3bd06d9082
MingseoYa <alstj3758@naver.com>
2025-02-09 14:47:18 +0900

    ✨ Feat: 마켓디테일 aside

---
+++ src/components/market-service/detail/aside/index.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { ExternalLink, MessageCircle } from "lucide-react";
+import styles from "./styles.module.css";
+import Image from "next/image";
+import { useState } from "react";
+import ContactModal from "../contact";
+import { MarketServiceDetailAsideProps } from "../types";
+
+export function MarketServiceDetailAside({
+  price,
+  seller,
+}: MarketServiceDetailAsideProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.card}>
+        <div className={styles.price}>{price}원</div>
+        <ul className={styles.warnings}>
+          <li>윈윈에서 포인트 충전 후 구매하실 수 있습니다.</li>
+          <li>상세 설명에 사용기한을 꼭 확인해 주세요.</li>
+        </ul>
+        <button className={styles.payment_button}>예약하기</button>
+      </div>
+      <div className={`${styles.card} ${styles.seller_card}`}>
+        <div className={styles.seller_info}>
+          <Image
+            src={
+              seller?.picture
+                ? `https://storage.googleapis.com/${seller.picture}`
+                : "/images/default-profile.png" // 기본 프로필 이미지
+            }
+            alt="판매자 프로필"
+            width={64}
+            height={64}
+            className={styles.seller_image}
+          />
+          <div>
+            <h3 className={styles.seller_name}>{seller?.name}</h3>
+            <p className={styles.seller_title}>{seller?.email}</p>
+          </div>
+        </div>
+        <a href="#" className={styles.portfolio_link}>
+          <ExternalLink className={styles.icon} />
+          포트폴리오 보기
+        </a>
+        <button className={styles.inquiry_button} onClick={toggleModal}>
+          <MessageCircle className={styles.icon} />
+          문의하기
+        </button>
+        {isModalOpen && (
+          <ContactModal
+            seller={seller}
+            isModalOpen={isModalOpen}
+            toggleModal={toggleModal}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
