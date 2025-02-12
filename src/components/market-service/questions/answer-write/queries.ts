cb3517ba513913215014f0c5cfefd3dfdab66465
MingseoYa <alstj3758@naver.com>
2025-02-12 21:05:04 +0900

    ✨ Feat: 마켓 서비스 리스트 페이지 퍼블리싱 및 api 연결

--- src/components/market-service/questions/answer-write/queries.ts
+++ src/components/market-service/questions/answer-write/queries.ts
@@ -1,25 +1,5 @@
 import { gql } from "@apollo/client";
 
-// 문의하기 보내기
-export const CREATE_TRAVEL_PRODUCT_QUESTION = gql`
-  mutation createTravelproductQuestion(
-    $createTravelproductQuestionInput: CreateTravelproductQuestionInput!
-    $travelproductId: ID!
-  ) {
-    createTravelproductQuestion(
-      createTravelproductQuestionInput: $createTravelproductQuestionInput
-      travelproductId: $travelproductId
-    ) {
-      _id
-      contents
-      user {
-        name
-      }
-      createdAt
-    }
-  }
-`;
-
 export const UPDATE_TRAVEL_PRODUCT_QUESTION = gql`
   mutation updateTravelproductQuestion(
     $updateTravelproductQuestionInput: UpdateTravelproductQuestionInput!
